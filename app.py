9e309fd29ad7d920f9119d321194c9bd4f106e3d
KL <kl@MacBook-Pro-cua-Le.local>
2026-03-19 16:28:10 +0700

    fix syntax

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def home():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get(\"PORT\", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
